9a34530f60e2bceb19e5dad6c28bf408504b0a1f
Nikolay Novikov <nikolaynovikov660@gmail.com>
2023-04-06 22:27:41 +0300

    доабвил плейбук рестарта

--- restart.groovy
+++ restart.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('restart') {
             steps {
-                ansiblePlaybook credentialsId: 'gitcred', vaultCredentialsId: 'vault1', disableHostKeyChecking: true, inventory: 'hosts', extraVars: '${role}', playbook: 'playbook_restart.yml'
+                ansiblePlaybook credentialsId: 'gitcred', vaultCredentialsId: 'vault1', disableHostKeyChecking: true, inventory: 'hosts', playbook: 'playbook_restart.yml -e role=${role}'
             }
         }
     }
