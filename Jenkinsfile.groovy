ef0bf8767a11bedc099a37a4c2876e0d40ca73a0
Nikolay Novikov <nikolaynovikov660@gmail.com>
2023-04-11 15:24:07 +0300

    удалил из роли шаги рестарта и доабвил вызов джобы рестарта

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent any
     stages {
+        stage('Stop_Bot') {
+            steps {
+                 build job: 'restart', parameters: [string(name: 'role', value: "stop")]
+}
+    }
         stage('update_bot') {
             steps {
                 ansiblePlaybook credentialsId: 'gitcred', vaultCredentialsId: 'vault1', disableHostKeyChecking: true, inventory: 'hosts', playbook: 'playbook.yml'
